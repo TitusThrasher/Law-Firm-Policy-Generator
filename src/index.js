0a697fbfaba68d0fae85d3fe99eafb901f0a4ce5
TitusThrasher <131217212+TitusThrasher@users.noreply.github.com>
2023-04-25 10:16:16 -0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app';
+
+const container = document.getElementById('root');
+ReactDOM.createRoot(container).render(<App />);
